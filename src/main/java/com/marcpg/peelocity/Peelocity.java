5d23b59747269008cbd360a153f6794be529d6bf
MarcPG1905 <marcgriepenkerl8@gmail.com>
2024-02-23 23:34:40 +0100

    0.1.9 - Build 2 - bStats Metrics
    - Added the bstats-velocity dependency and added a relocation for it.
    - Added support for the Metrics and added all charts.
    - Added javadocs to the methods in Peelocity.
    - Changed some relocations to make more sense.

--- src/main/java/com/marcpg/peelocity/Peelocity.java
+++ src/main/java/com/marcpg/peelocity/Peelocity.java
@@ -30,6 +30,8 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.bstats.charts.SimplePie;
+import org.bstats.velocity.Metrics;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
@@ -49,7 +51,7 @@ public class Peelocity {
     @SuppressWarnings("unused")
     public enum ReleaseType { ALPHA, BETA, SNAPSHOT, PRE, RELEASE }  public static final ReleaseType PEELOCITY_RELEASE_TYPE = ReleaseType.BETA;
     public static final String PEELOCITY_VERSION = "0.1.9";
-    public static final String PEELOCITY_BUILD_NUMBER = "1";
+    public static final String PEELOCITY_BUILD_NUMBER = "2";
 
     public static final List<String> COMMANDS = List.of("announce", "ban", "config", "friend", "hub", "join",
             "kick", "message-history", "msg", "mute", "pardon", "party", "peeload", "report", "staff", "unmute", "w");
@@ -58,14 +60,16 @@ public class Peelocity {
     public static ProxyServer SERVER;
     public static Logger LOG;
     public static Path DATA_DIRECTORY;
+    public static Metrics.Factory METRICS_FACTORY;
 
 
     @Inject
-    public Peelocity(@NotNull ProxyServer server, @NotNull Logger logger, @DataDirectory Path dataDirectory) {
+    public Peelocity(@NotNull ProxyServer server, @NotNull Logger logger, @DataDirectory Path dataDirectory, Metrics.Factory metricsFactory) {
         PLUGIN = this;
         SERVER = server;
         LOG = logger;
         DATA_DIRECTORY = dataDirectory;
+        METRICS_FACTORY = metricsFactory;
     }
 
     @Subscribe
@@ -75,19 +79,38 @@ public class Peelocity {
         Config.createDataDirectory();
         Config.load(getClass().getResourceAsStream("/pee.yml"));
 
-        PlayerCache.load();
+        PlayerCache.load(); // Loads all players into cache
 
         if (Config.STORAGE_TYPE == Storage.StorageType.DATABASE)
-            DatabaseStorage.loadDependency();
+            DatabaseStorage.loadDependency(); // Downloads and loads the database's JDBC driver
 
-        registerEvents(SERVER.getEventManager());
-        registerCommands(SERVER.getCommandManager());
-        sendWelcome(SERVER.getConsoleCommandSource());
+        bStatsMetrics(21102); // Sends the metrics on bStats
+        registerEvents(SERVER.getEventManager()); // Registers the events, if enabled
+        registerCommands(SERVER.getCommandManager()); // Registers the commands, if enabled
+        sendWelcome(SERVER.getConsoleCommandSource()); // Sends the cool little info message
         LOG.info(Ansi.formattedString("Loaded all components, took " + (System.currentTimeMillis() - start) + "ms!", Ansi.GREEN));
     }
 
+    /**
+     * Sends all metrics about the current instance to the bStats plugin.
+     * @param pluginId The plugin's ID on bStats.
+     */
+    public void bStatsMetrics(int pluginId) {
+        Metrics metrics = METRICS_FACTORY.make(this, pluginId);
+        metrics.addCustomChart(new SimplePie("storage_method", () -> Config.STORAGE_TYPE.toString().toLowerCase()));
+        metrics.addCustomChart(new SimplePie("server_list", () -> String.valueOf(Config.SL_ENABLED)));
+        metrics.addCustomChart(new SimplePie("chat_utils", () -> String.valueOf(Config.CHATUTILITY_BOOLEANS.getBoolean("enabled"))));
+        metrics.addCustomChart(new SimplePie("translations", () -> String.valueOf(Config.CONFIG.getBoolean("enable-translations"))));
+        metrics.addCustomChart(new SimplePie("whitelist", () -> String.valueOf(Config.WHITELIST)));
+    }
+
+    /**
+     * Goes through all events and registers them, if they need to be registered.
+     * @param manager The instance's event manager, that events are registered on.
+     */
     public void registerEvents(@NotNull EventManager manager) {
         manager.unregisterListeners(this);
+
         if (Config.CHATUTILITY_BOOLEANS.getBoolean("enabled")) manager.register(this, new ChatUtilities());
         if (Config.SL_ENABLED) manager.register(this, new ServerList());
         if (Config.WHITELIST) manager.register(this, new Whitelist());
@@ -97,6 +120,10 @@ public class Peelocity {
         manager.register(this, new Mutes());
     }
 
+    /**
+     * Goes through all commands and registers them, if they need to be registered.
+     * @param manager The instance's command manager, that commands are registered on.
+     */
     public void registerCommands(@NotNull CommandManager manager) {
         if (manager.hasCommand("announce")) COMMANDS.forEach(manager::unregister);
 
@@ -144,6 +171,14 @@ public class Peelocity {
         }, "velocity-plugin");
     }
 
+    /**
+     * Sends this welcome message with the PEE text using {@code |} and {@code _} into the specified console:
+     * <pre>      __   __  __
+     *    |__) |__ |__ Peelocity
+     *    |    |__ |__ <a href="https://marcpg.com/peelocity">https://marcpg.com/peelocity</a>
+     *    Build: VERSION-BUILD_NR (RELEASE_TYPE)</pre>
+     * @param console The console (-command source) to send the message to.
+     */
     static void sendWelcome(@NotNull ConsoleCommandSource console) {
         console.sendMessage(Component.text("    __   __  __", NamedTextColor.YELLOW));
         console.sendMessage(Component.text("   |__) |__ |__ Peelocity " + PEELOCITY_VERSION, NamedTextColor.YELLOW));
@@ -156,6 +191,11 @@ public class Peelocity {
         PlayerCache.save();
     }
 
+    /**
+     * Logs an important error to the {@link Peelocity#LOG instance's logger}, that is hard to overlook. <br>
+     * Should only be used for really important things, that really need to be seen by the user.
+     * @param msg A brief description of what went wrong.
+     */
     public static void importantError(String msg) {
         msg = " " + msg + " ";
         Peelocity.LOG.error("----------------------------------------------------------------------------------------");
